a1bed284081b8d6db52aec0bd6574e87cc09a613
bismarckparentedev-bit <bismarckparente.dev@gmail.com>
2026-05-15 11:21:40 -0300

    feat(api): change greeting to localized message (#1)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+  res.json({ message: "Olá mundoo" });
 });
 
 app.listen(PORT, () => {
